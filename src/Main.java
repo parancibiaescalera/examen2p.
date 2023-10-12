8f7d7c798794865fd1d5ae8aa35eefa0c69ad25c
pablo <polarancibia.27@gmail.com>
2023-10-12 09:43:26 -0400

    examen completado

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Suma sum1=new Suma();
+        sum1.sumar(23,28);
+        Suma sum2=new Suma();
+        sum1.sumar(9,5,10);
+        Suma sum3=new Suma();
+        sum1.sumar(2.1,2.1);
+        Suma sum4=new Suma();
+        sum1.sumar(2.6,4.9,12.44);
+
+
+
+    }
+}
---
+++ src/Suma.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class Suma {
+    void sumar(int valorA, int valorB){
+        System.out.println("la suma de dos valores es:"+(valorA+valorB));
+    }
+    void sumar(int valorA, int valorB, int valorC){
+        System.out.println("la suma de tres valores es:"+(valorA+valorB+valorC));
+    }
+    void sumar(double valorA, double valorB){
+        System.out.println("la suma dos valores con decimales es:"+(valorA+valorB));
+    }
+    void sumar(double valorA, double valorB, double valorC){
+        System.out.println("la suma tres valores con decimales es:"+(valorA+valorB+valorC));
+    }
+}
